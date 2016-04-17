e1837c8bead9e0caef0dee51cecff3ea637522c7
MrStrings <phstring95@hotmail.com>
2016-04-17 19:16:02 -0300

    splash screen

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	float foundEnemy;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
